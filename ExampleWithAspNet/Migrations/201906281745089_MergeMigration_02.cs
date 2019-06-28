62e42e62a514f421711ed386370ac0a8e48b5e1f
Stepan Ugrin <stepan.ugrin1@gmail.com>
2019-06-28 20:46:46 +0300

    Add MergeMigration_02

---
+++ ExampleWithAspNet/Migrations/201906281745089_MergeMigration_02.cs
@@ -0,0 +1,18 @@
+namespace ExampleWithAspNet.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MergeMigration_02 : DbMigration
+    {
+        public override void Up()
+        {
+            
+        }
+        
+        public override void Down()
+        {
+            
+        }
+    }
+}
